36e88deaa7a63feeb0c959540e1d9f98281623c4
Sanjay Mahto <mahto.sanjay41@gmail.com>
2019-04-09 14:22:18 +0530

    server url changed.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const ROOT = 'dummy'
-export const SERVER_URL = "http://localhost:5000";
+export const SERVER_URL = "http://ec2-54-169-181-35.ap-southeast-1.compute.amazonaws.com:5000";
 export const FETCH_FOLDER_CONTENTS="/api/files";
 export const CREATE_FILE="/api/createFile";
 export const CREATE_FOLDER="/api/createFolder";
